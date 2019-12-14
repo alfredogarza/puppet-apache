1c29904c8741d448c8521792c7948838069719e6
root <root@sysenglearning3c.mylabserver.com>
2019-12-14 00:40:07 +0000

    Refactored params.pp in favor of Hiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,11 +2,8 @@
 #
 # installs the base Apache package.
 #
-class apache::install (
-  $install_name   = $apache::params::install_name,
-  $install_emsure = $apache::params::install_emsure,
-) inherits apache::params {
-  package { "${install_name}":
-    ensure => $install_ensure,
+class apache::install { 
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
